15e800dcc8400558c2df099cda76182c47220e1c
faithwest <faithchepkoech425@gmail.com>
2023-09-25 22:21:51 +0300

    salary calculator

--- solution.js/salary.js
+++ solution.js/salary.js
@@ -1,75 +1,64 @@
-//function to calculate KRA/PAYE,deductions per grouped salary
-//also shows the tax rate in percentage
+// Define tax rates and deduction limits
+const taxRates = [
+  { min: 0, max: 24000, rate: 0.1 },    // 10% tax rate
+  { min: 24001, max: 32333, rate: 0.25 }, // 25% tax rate
+  { min: 32334, max: 500000, rate: 0.3 }, // 30% tax rate
+  { min: 500001, max: 800000, rate: 0.325 }, // 32.5% tax rate
+  { min: 800001, max: Infinity, rate: 0.35 } // 35% tax rate
+];
 
+const nhifDeductions = [
+  { min: 0, max: 5999, deduction: 150 },
+  { min: 6000, max: 7999, deduction: 300 },
+  { min: 8000, max: 11999, deduction: 400 },
+  { min: 12000, max: 14999, deduction: 500 },
+  { min: 15000, max: 19999, deduction: 600 },
+  { min: 20000, max: 24999, deduction: 750 },
+  { min: 25000, max: 29999, deduction: 850 },
+  { min: 30000, max: 34999, deduction: 900 },
+  { min: 35000, max: 39999, deduction: 950 },
+  { min: 40000, max: Infinity, deduction: 1000 }
+];
+
+// Function to calculate PAYE based on monthly taxable pay
 function calculatePAYE(monthlyTaxablePay) {
-    if (monthlyTaxablePay <= 24000) {
-      return monthlyTaxablePay * 0.1; // 10% 
-    } else if (monthlyTaxablePay <= 32333) {
-      return monthlyTaxablePay * 0.25; // 25% 
-    } else if (monthlyTaxablePay <= 500000) {
-      return monthlyTaxablePay * 0.3; // 30% 
-    } else if (monthlyTaxablePay <= 800000) {
-      return monthlyTaxablePay * 0.325; // 32.5% 
-    } else {
-      return monthlyTaxablePay * 0.35; // 35% 
+  for (const rate of taxRates) {
+    if (monthlyTaxablePay >= rate.min && monthlyTaxablePay <= rate.max) {
+      return monthlyTaxablePay * rate.rate;
     }
   }
-  
-  //function to calculate NHIF deductions per grouped salary
-  function calculateNHIF(grossPay) {
-    if (grossPay <= 5999) {
-      return 150;
-    } else if (grossPay <= 7999) {
-      return 300;
-    } else if (grossPay <= 11999) {
-      return 400;
-    } else if (grossPay <= 14999) {
-      return 500;
-    } else if (grossPay <= 19999) {
-      return 600;
-    } else if (grossPay <= 24999) {
-      return 750;
-    } else if (grossPay <= 29999) {
-      return 850;
-    } else if (grossPay <= 34999) {
-      return 900;
-    } else if (grossPay <= 39999) {
-      return 950;
-    } else {
-      return 1000;
+  return 0;
+}
+
+// Function to calculate NHIF deduction based on gross pay
+function calculateNHIF(grossPay) {
+  for (const deduction of nhifDeductions) {
+    if (grossPay >= deduction.min && grossPay <= deduction.max) {
+      return deduction.deduction;
     }
   }
-  
+  return 0;
+}
 
-  //function to calculate NSSF deductions per salried group 
-  //NSSF deductions has a standard rate of 6% of the employees' salary
-  function calculateNSSF(pensionablePay) {
-    if (pensionablePay <= 6000) {
-      return pensionablePay * 0.06; // 6% 
-    } else {
-      return 18000 * 0.06 + (pensionablePay - 6000) * 0.06; // Contributions for Tiers I and II
-    }
-  }
-  
-  //function for the newly implemented housing levy
-  function calculateHousingLevy(grossPay) {
-    return grossPay * 0.015; // 1.5% Housing Levy
-  }
+// Function to calculate NSSF deduction (assuming 6% of pensionable pay)
+function calculateNSSF(pensionablePay) {
+  const rate = 0.06; // 6% rate
+  return pensionablePay * rate;
+}
 
-  //use this example to calculate the deductions for each section
+// Function to calculate Housing Levy (1.5% of gross pay)
+function calculateHousingLevy(grossPay) {
+  const rate = 0.015; // 1.5% rate
+  return grossPay * rate;
+}
 
-  const paye = calculatePAYE(monthlyTaxablePay); 
-  console.log(`PAYE: ${paye}`);
-
-  const nhif = calculateNHIF(grossPay); 
-  console.log(`NHIF: ${nhif}`);
-
-  const nssf = calculateNSSF(pensionablePay);
-  console.log(`NSSF: ${nssf}`);
-
+// Function to calculate net salary
+function calculateNetSalary(grossPay, monthlyTaxablePay) {
+  const paye = calculatePAYE(monthlyTaxablePay);
+  const nhif = calculateNHIF(grossPay);
+  const nssf = calculateNSSF(grossPay);
   const housingLevy = calculateHousingLevy(grossPay);
-  console.log(`Housing Levy: ${housingLevy}`);
- 
- 
-  
-  
+
+  const netSalary = grossPay - (paye + nhif + nssf + housingLevy);
+  return netSalary;
+}
